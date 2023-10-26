e388020096bd3d7e7918d05fc0846dea0763aab8
jhigger <njohnjhigger@gmail.com>
2023-10-26 09:19:45 +0800

    change image source

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -1,12 +1,14 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
+
 const HeroImage = () => {
 	return (
 		<div className="relative w-full py-16 opacity-100 lg:py-28">
 			<div className="absolute top-0 left-0 h-[100vw] w-full md:h-[32rem] xl:h-[40rem]">
-				<img
+				<Image
 					className="h-full w-full object-cover object-center"
-					src="https://utilityape.com/assets/images/backgroup/bg-1.jpg"
+					src="/assets/bg-1.jpg"
 					alt="Utility APE"
+					layout="fill"
 				/>
 				<div className="absolute top-0 left-0 z-10 h-full w-full bg-gradient-to-b from-transparent to-black"></div>
 			</div>
